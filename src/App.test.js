dc70a32488975b45d755cfafbf40ccf8f97f5759
Brian Bolnick <brianbolnick@gmail.com>
2020-12-03 15:10:34 -0700

    add dummy test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders the home page with a title', () => {
+  render(<App />, { wrapper: MemoryRouter });
+  const titleElement = screen.getByText(/Top 100 Movies/i);
+  expect(titleElement).toBeInTheDocument();
 });
